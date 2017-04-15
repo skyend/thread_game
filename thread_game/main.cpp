89d483faa78b70d3b2bccd8abfa2a060666ecf1b
Jinwoong Han <theskyend0@gmail.com>
2017-04-15 14:30:49 +0900

    Initial Commit

---
+++ thread_game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  thread_game
+//
+//  Created by Jin Woong Han on 2017. 4. 15..
+//  Copyright © 2017년 skyend. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
